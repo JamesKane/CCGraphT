c36ebe0672569a155c2d010f9cb8d5456e6e0a49
James Kane <jkane@tds.net>
2012-11-16 18:46:06 -0600

    Adding BFS

---
+++ CCGraphT/Traversal/CCBreadthFirstIterator.h
@@ -0,0 +1,13 @@
+//
+//  CCBreadthFirstIterator.h
+//  CCGraphT
+//
+//  Created by James Kane on 11/16/12.
+//  Copyright (c) 2012 James Kane. All rights reserved.
+//
+
+#import "CCCrossComponentIterator.h"
+
+@interface CCBreadthFirstIterator : CCCrossComponentIterator
+
+@end
